0f6539b19dbd85a0ff3cce2f46f4ce866bfa5027
nightwishh <65540986+nightwishh@users.noreply.github.com>
2024-09-16 10:35:25 +0400

    n

--- backend/app.js
+++ backend/app.js
@@ -1,48 +1,74 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const User = require("./database");
-const passport = require('passport')
-const session = require('express-session')
-const {hashSync} = require('bcrypt');
-const MongoStore = require('connect-mongo');
-const cors = require('cors');
+const passport = require("passport");
+const session = require("express-session");
+const { hashSync } = require("bcrypt");
+const MongoStore = require("connect-mongo");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+require("./passport")(passport);
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(cors({
-  origin: 'http://localhost:4200',
-  credentials: true
-}));
-app.use(session({
-  secret: "secret",
-  resave: false ,
-  saveUninitialized: true ,
-  store: MongoStore.create({mongoUrl: process.env.DATABASE_URL,colletionName:"sessions"}),
-  cookie:{
-    secure: true,
-    sameSite: 'none'
-  }
-}));
-app.use(passport.initialize()) 
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.DATABASE_URL,
+      colletionName: "sessions",
+    }),
+  })
+);
+app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  console.log("Session:", req.session);
+  console.log("User:", req.user);
+  console.log("Is Authenticated:", req.isAuthenticated());
+
+  req.isAuthenticated() ? res.send("Hello world") : res.send("Try logging in");
 });
 app.post("/register", async (req, res) => {
   const user = new User({
     fullName: req.body.fullName,
     email: req.body.email,
-    password: hashSync(req.body.password,10),
+    password: hashSync(req.body.password, 10),
   });
-  // await user.save();
+  await user.save();
   res.json(user);
 });
-app.post("/login", passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login'
-}));
+app.get("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+    if (!user) {
+      return res.status(401).json({ message: "Authentication failed" });
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return res.status(500).json({ message: "Internal Server Error" });
+      }
+      console.log("User authenticated:", req.isAuthenticated());
+
+      return res
+        .status(200)
+        .json({ message: "Authentication successful", user: user });
+    });
+  })(req, res, next);
+});
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
--- backend/passport.js
+++ backend/passport.js
@@ -1,25 +1,31 @@
-const passport = require("passport"), LocalStrategy = require("passport-local").Strategy;
-const { compareSync } = require("bcrypt");
-const User =  require("./database");
-passport.use(new LocalStrategy,(function(username,password,done) {
-    User.findOne({email: username,password: password}, function(err,user) {
-        if (err) {
-            return done(err);
-        }
-        if (!user) {
-            return done(null, false, { message: 'Incorrect username.' });
-        }
-        if (!compareSync(user.password, password)) {
-            return done(null, false, { message: 'Incorrect password.' });
-        }
-        return done(null, user);
-    });
-}));
-passport.serializeUser((user, done) => {
+const passport = require("passport"),
+  LocalStrategy = require("passport-local").Strategy;
+const { hashSync, compareSync } = require("bcrypt");
+const User = require("./database");
+module.exports = function (passport) {
+  passport.use(
+    new LocalStrategy(async function (username, password, done) {
+      var user;
+      try {
+        user = await User.findOne({ email: username });
+      } catch (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username." });
+      }
+      if (!compareSync(password, user.password)) {
+        return done(null, false, { message: "Incorrect password." });
+      }
+      return done(null, user);
+    })
+  );
+  passport.serializeUser((user, done) => {
     done(null, user.id);
-});
-passport.deserializeUser((id, done) => {
+  });
+  passport.deserializeUser((id, done) => {
     User.findById(id, (err, user) => {
-        done(err, user);
+      done(err, user);
     });
-});
+  });
+};
